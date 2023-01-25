ca15aa04c3598d815229db0439b7856a9d4b0a60
VictoriaRob3 <victoria.nguyen@etu.upmc.fr>
2023-01-25 15:12:39 +0100

    first commit

---
+++ launch/listener.launch.py
@@ -0,0 +1,12 @@
+# lance le listener node
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_py',
+            executable='listener'
+        )
+    ])
---
+++ launch/talker.launch.py
@@ -0,0 +1,13 @@
+# lance le talker node
+
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_cpp',
+            executable='talker'
+        )
+    ])
